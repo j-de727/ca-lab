9f179d7eb3f9fa513593ccc1e057046891d10836
luciewang <wangyiduo0310@gmail.com>
2025-10-21 11:55:47 -0400

    Completed LoggedInView.java.

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -110,6 +110,9 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
     public void actionPerformed(ActionEvent evt) {
         // TODO: execute the logout use case through the Controller
         System.out.println("Click " + evt.getActionCommand());
+        if (evt.getSource() == logOut && logoutController != null) {
+            logoutController.execute();
+        }
     }
 
     @Override
@@ -141,5 +144,6 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
 
     public void setLogoutController(LogoutController logoutController) {
         // TODO: save the logout controller in the instance variable.
+        this.logoutController = logoutController;
     }
 }
